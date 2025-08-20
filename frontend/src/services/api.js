510b3e72bd61743c0e310f61405bc24461e1eae1
James Han <hanlyu2005@gmail.com>
2025-08-20 14:58:49 -0400

    updated backend api url

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,4 +1,4 @@
-const API_BASE_URL = 'http://localhost:8000';
+const API_BASE_URL = 'https://trinity-college-orientation-backend-production.up.railway.app';
 
 export const apiService = {
   // Fetch all events
